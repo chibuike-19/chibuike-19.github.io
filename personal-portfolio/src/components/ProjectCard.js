9ca2fa29b652f359b0f0a42de7b64d43a653c942
Chibuike-19 <emmanuelobiechina8@gmail.com>
2022-12-03 13:39:58 +0100

    new commit

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -7,12 +7,12 @@ export const ProjectCard = ({ title, description, imgUrl, tags, sourceCode, live
       <div className="proj-imgbx">
         <img src={imgUrl} className='proj-img' alt='project images' />
         <div className="proj-txtx">
-          <div><h4>{title}</h4>
+          <div className="text-summary"><div><h4>{title}</h4>
           <span>{description}</span><br/>
           </div>
           <div className="tags">{tags.map(tag => (
             <TagWrapper tags={tag} />
-          ))}</div>
+          ))}</div></div>
           <div className="link-container">
             <span className="project-link"><a href={sourceCode} target='_blank'>Source Code</a></span>
             <span className="project-link"><a href={liveDemo} target='_blank'>Live Demo</a></span>
